77c8d2c64d894df3a8b785aefca5b37c3247b652
Margaret Gu <margaretgu@Margarets-MacBook-Air.local>
2018-12-08 22:44:34 -0500

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SignupComponent } from './signup/signup.component';
+import { PageNotFoundComponent } from './PageNotFound/error.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SignupComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule
